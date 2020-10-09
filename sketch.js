a704e7a71b6493dc72e4f94fa3b38f1093d78ef4
Abhinay <vijayabalabhadra14@gmail.com>
2020-10-09 16:42:29 +0530

    mouseevents

--- sketch.js
+++ sketch.js
@@ -39,7 +39,7 @@ function setup(){
     bird = new Bird(100,100);
 
     log6 = new Log(230,180,80, PI/2);
-    chain = new Chain(bird.body,log6.body);
+    slingShot = new Slingshot(bird.body,{x:200,y:100});
 }
 
 function draw(){
@@ -63,6 +63,21 @@ function draw(){
 
     bird.display();
     platform.display();
-    log6.display();
-    chain.display();    
+    //log6.display();
+    slingShot.display();  
+    
+    
+}
+
+function mouseDragged(){
+    Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY});
+
+
+}
+
+function mouseReleased(){
+    slingShot.fly();
+
+    
+
 }
